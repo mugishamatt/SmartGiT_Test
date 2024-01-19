1f83fa165fee2e8b3b174794823697ebf52df365
mattmugisha <mugisham410@gmail.com>
2024-01-18 23:20:48 -0600

    first test commit

---
+++ SmartGitTest/src/justtest/Animal.java
@@ -0,0 +1,13 @@
+package justtest;
+
+public class Animal {
+	String name;
+	String type;
+	String email;
+
+	public static void main(String[] args) {
+		System.out.println("just tesing");
+
+	}
+
+}
---
+++ SmartGitTest/src/justtest/NewClass.java
@@ -0,0 +1,5 @@
+package justtest;
+
+public class NewClass {
+	int age;
+}
